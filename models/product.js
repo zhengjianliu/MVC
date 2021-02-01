d6ac415e532522bcccc409a3f5507f51347cb6aa
zhengjianliu <comezheng@gmail.com>
2021-01-31 19:23:21 -0500

    update

--- models/product.js
+++ models/product.js
@@ -1,5 +1,19 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+const p = path.join(path.dirname(process.mainModule.filename),
+'data',
+'products.json'
+)
 
+const getProductsFromFile = (cb) =>{
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return cb([])
+            }else{
+                cb(JSON.parse(fileContent))
+            }
+        })
+}
 
 module.exports = class Product {
     constructor(t){
@@ -7,10 +21,15 @@ module.exports = class Product {
     }
 
     save(){
-        products.push(this)
+        getProductsFromFile(products =>{
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products),err=>{
+                console.log(err)
+            })
+        })
     }
 
-    static fetchAll(){
-        return products
+    static fetchAll(cb){
+        getProductsFromFile(cb)
     }
 }
